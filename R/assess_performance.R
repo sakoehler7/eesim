ab6b92e5d80298fe5662670f20e9e3172e3f64dc
sarahkoehler <sakoehler7@gmail.com>
2016-06-20 15:58:16 -0600

    Fixing beta_bias, adding coverage_beta

--- R/assess_performance.R
+++ R/assess_performance.R
@@ -40,13 +40,42 @@ beta_var <- function(df){
   return(out)
 }
 #'
+#' Percent Bias of Estimated Coefficient
+#'
+#' This function returns the relative bias of the mean of the estimated coefficients.
+#'
+#' @inheritParams mean_beta
+#' @param true_rr The true relative risk used to simulate your data
+#'
+#' @return The percent bias of the mean of the estimated coefficients over n simulations
+#'
+#' @examples
+#' beta_bias(df, true_rr = 1.02)
+#'
+beta_bias <- function(df, true_rr){
+  percent_bias <- 100 * (true_rr - mean(exp(df$est))) / true_rr
+  out <- data.frame(percent_bias)
+  return(out)
+}
+#'
 #' Percent Coverage of Estimated Coefficients
 #'
-#' This function gives the percent coverage for the true coefficient.
+#' This function gives the percent coverage of the true coefficient.
 #'
-#' @param df A data frame of replicated simulations which must include columns titled
+#' @param df A data frame of replicated simulations which must include columns titled "lower_ci" and "upper_ci"
+#' @param true_rr The true relative risk used to simulate your data
 #'
+#' @return The percent of confidence intervals for the estimated relative risk over n simulations which include the true relative risk
 #'
+#' @examples
+#' coverage_beta(df, true_rr = 1.02)
+#'
+coverage_beta <- function(df, true_rr){
+  true_beta <- log(true_rr)
+  coverage <- df$lower_ci <= true_beta & df$upper_ci >= true_beta
+  out <- data.frame(coverage = sum(coverage) / nrow(df))
+  return(out)
+}
 #'
 #'
 #'
