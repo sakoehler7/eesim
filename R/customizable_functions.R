f84e2755947dfc5c26ec235c6e63770f819b8351
sarahkoehler <sakoehler7@gmail.com>
2016-08-16 14:40:13 -0600

    Fix sim_outcome to include 2 stages of customization

--- R/customizable_functions.R
+++ R/customizable_functions.R
@@ -10,17 +10,19 @@ custom_exposure <- function(n, df = dlnm::chicagoNMMAPS, central = NA,
 
 #' Simulate random series of exposure values
 #'
+#' @param cust_exp_args A list of arguments used in the user-specified custom function
+#'
 #' @examples
 #' sim_exposure(n = 5, central = 0.25, exposure_type = "binary", amp = .02)
 #' sim_exposure(n = 5, central = 100, sd = 10, amp = .6,
 #'              exposure_type = "continuous")
-#' sim_exposure(n = 5, central = NA, custom_func = "custom_exposure",
-#'                     metric = "temp")
+#' sim_exposure(n = 5, cust_exp_func = "custom_exposure",
+#'                     cust_exp_args = metric = "temp")
 #'
 #' @export
 sim_exposure <- function(n, central = NULL, trend = "no trend", amp = .6,
                          exposure_type = NULL,
-                         start.date = "2001-01-01", custom_func = NULL,
+                         start.date = "2001-01-01", cust_exp_func = NULL,
                          cust_exp_args = NULL){
   if(is.null(cust_exp_args)){
     arguments <- vector(mode = "list")
@@ -29,7 +31,7 @@ sim_exposure <- function(n, central = NULL, trend = "no trend", amp = .6,
   }
   arguments$n <- n
 
-  if(is.null(custom_func)){
+  if(is.null(cust_exp_func)){
     if(is.null(central)){
       stop(paste0("If a custom function is not used to generate exposure values, a central value must be specified."))
     }
@@ -38,17 +40,17 @@ sim_exposure <- function(n, central = NULL, trend = "no trend", amp = .6,
     arguments$amp <- amp
     arguments$exposure_type <- exposure_type
     exposure <- do.call("std_exposure", arguments)
-  } else if (!(is.null(custom_func))){
+  } else if (!(is.null(cust_exp_func))){
     if (!is.null(central)){
       arguments$central <- central
     }
     start.date <- as.Date(start.date)
     date <- seq(from = start.date, by = 1, length.out = n)
-    x <- do.call(custom_func, arguments)
+    x <- do.call(cust_exp_func, arguments)
     exposure <- data.frame(date, x)
   } else {
     stop(paste0("If a custom function is not used to simulate randomness in the
-                exposure variable, then the parameters central and
+                exposure variable, then central and
                 exposure_type must be specified."))
   }
   return(exposure)
@@ -79,9 +81,9 @@ custom_baseline <- function(n, df = dlnm::chicagoNMMAPS, average_outcome = NA,
 #' create_baseline(n = 5, average_outcome = NA, trend = NA, amp = NA,
 #'                 custom_func = "custom_baseline", outcome_type = "death")
 #'
-create_baseline <- function(n, average_outcome, trend, amp, custom_func = NULL,
+create_baseline <- function(n, average_outcome, trend, amp, cust_base_func = NULL,
                             ...){
-  if(is.null(custom_func)){
+  if(is.null(cust_base_func)){
     lambda <- average_outcome
     baseline <- sim_baseline(n=n, lambda=lambda, trend=trend, amp=amp)
   } else {
@@ -89,13 +91,13 @@ create_baseline <- function(n, average_outcome, trend, amp, custom_func = NULL,
     arguments$n <- n
     arguments$average_outcome <- average_outcome
     arguments$trend <- trend
-    baseline <- do.call(custom_func, arguments)
+    baseline <- do.call(cust_base_func, arguments)
   }
   return(baseline)
 }
 
-create_lambda <- function(baseline, exposure, rr, custom_func = NULL, ...){
-  if(is.null(custom_func)){
+create_lambda <- function(baseline, exposure, rr, cust_lambda_func = NULL, ...){
+  if(is.null(cust_lambda_func)){
     log_lambda <- log(baseline) + log(rr) * exposure
     lambda <- exp(log_lambda)
   } else {
@@ -103,21 +105,28 @@ create_lambda <- function(baseline, exposure, rr, custom_func = NULL, ...){
     arguments$baseline <- baseline
     arguments$exposure <- exposure
     arguments$rr <- rr
-    lambda <- do.call(custom_func, arguments)
+    lambda <- do.call(cust_lambda_func, arguments)
   }
   return(lambda)
 }
-
+#' @param cust_args A list of arguments and their values used in the user-specified custom functions
 #' @export
 
 sim_outcome <- function(exposure, average_outcome = NULL, trend = "no trend",
                         amp = .6, rr = 1.01, start.date="2000-01-01",
-                        custom_func = NULL, ...){
+                        cust_base_func = NULL, cust_lambda_func = NULL,
+                        cust_args = NULL,...){
   start.date <- as.Date(start.date)
   date <- seq(from = start.date, by = 1, length.out = nrow(exposure))
-  if(is.null(custom_func)){
+  if (is.null(cust_args)){
+    arguments <- vector(mode = "list")
+  } else {
+    arguments <- cust_args
+  }
+  arguments$n <- n
+  if(is.null(cust_base_func) & is.null(cust_lambda_func)){
     if(is.null(average_outcome)){
-      stop(paste0("If a custom function is not used to generate outcomes,
+      stop(paste0("If custom functions are not used to generate outcomes,
                   a value for average_outcome must be specified."))
     }
     baseline <- create_baseline(n = nrow(exposure),
@@ -129,15 +138,37 @@ sim_outcome <- function(exposure, average_outcome = NULL, trend = "no trend",
                             rr = rr)
     outcome <- rpois(n = nrow(exposure), lambda = lambda)
   }
-  else {
-    arguments <- list(...)
-    arguments$n <- n
+  else if (is.null(cust_lambda_func) & !is.null(cust_base_func)){
     arguments$average_outcome <- average_outcome
-    arguments$trend <- trend
-    arguments$baseline <- baseline
     arguments$exposure <- exposure
-    arguments$lambda <- lambda
-    outcome <- do.call(custom_func, arguments)
+    arguments$rr <- rr
+    baseline <- do.call(cust_base_func, arguments)
+    lambda <- create_lambda(baseline = baseline$exp_base_y,
+                            exposure = exposure$x,
+                            rr = rr)
+    outcome <- rpois(n = nrow(exposure), lambda = lambda)
+  }
+  else if (is.null(cust_base_func) & !is.null(cust_lambda_func)){
+    arguments$average_outcome <- average_outcome
+    arguments$exposure <- exposure
+    arguments$rr <- rr
+    arguments$trend <- trend
+    arguments$amp <- amp
+    baseline <- create_baseline(n = nrow(exposure),
+                                average_outcome = average_outcome,
+                                trend = trend,
+                                amp = amp)
+    lambda <- do.call(cust_lambda_func, arguments)
+    outcome <- rpois(n = nrow(exposure), lambda = lambda)
+  }
+  else {
+    arguments$average_outcome <- average_outcome
+    arguments$exposure <- exposure
+    arguments$rr <- rr
+    baseline <- do.call(cust_base_func, arguments)
+    arguments$baseline <- baseline
+    lambda <- do.call(cust_lambda_func, arguments)
+    outcome <- rpois(n = nrow(exposure), lambda = lambda)
   }
   df <- data.frame(date, x = exposure$x, outcome)
   return(df)
