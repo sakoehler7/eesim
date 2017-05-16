3191865653e550e489308d38af291dcf80e521b6
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-16 09:33:46 -0600

    Change to use second argument of `eesim` output for coverage plot examples

--- R/plots.R
+++ R/plots.R
@@ -18,10 +18,8 @@
 #' @examples
 #' ex_sim <- eesim(n_reps = 100, n = 1000, central = 100, sd = 10,
 #'           exposure_type = "continuous", average_outcome = 20, rr = 1.02,
-#'           model = "spline", df_year = 1)
-#' coverage_plot(ex_sim[[1]], true_param = 1.02)
-#'
-#' @importFrom dplyr %>%
+#'           custom_model = spline_mod, custom_model_args = list(df_year = 1))
+#' coverage_plot(ex_sim[[2]], true_param = 1.02)
 #'
 #' @export
 #'
